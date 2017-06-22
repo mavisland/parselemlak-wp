e15bd4e8154a80a11c099970dfc22c155bd94e67
Tanju Yildiz <ben@tanjuyildiz.com>
2017-06-22 13:58:13 +0300

    Icerik sayfalari featured image alanlari duzenlendi.

--- single.php
+++ single.php
@@ -1,27 +1,34 @@
 <?php get_header(); ?>
 	<div class="site-content">
+<?php while ( have_posts() ) : the_post(); ?>
 		<div class="section main-content">
 			<div class="wrapper">
 				<div class="container-fluid">
 					<div class="row">
-						<div class="col-xs-12 col-sm-9 col-sm-push-3">
-<?php while ( have_posts() ) : the_post(); ?>
-							<img class="img-responsive" src="http://placehold.it/900x300" alt="">
-							<h1><?php the_title(); ?></h1>
-							<?php the_content(); ?>
-							<hr>
-							<div class="sosyal">
-								<div class="sosyal1">
-									<a href="https://twitter.com/share" class="twitter-share-button" data-lang="tr">Tweet</a>
-									<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
-								</div>
-								<div class="sosyal2">
-									<iframe src="//www.facebook.com/plugins/like.php?href=<?php the_permalink(); ?>&amp;width=468&amp;layout=standard&amp;action=like&amp;show_faces=false&amp;share=true&amp;height=20" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:468px; height:20px;" allowTransparency="true"></iframe>
-								</div>
+<?php if ( has_post_thumbnail() ) : ?>
+						<div class="col-xs-12">
+							<div class="featured-image">
+								<?php the_post_thumbnail("full", [ "class" => "img-responsive", "alt" => get_the_title() ]); ?>
 							</div>
-							<hr>
-							<p class="small text-muted"><span class="fa fa-time"></span> <?php the_date(); ?></p>
-<?php endwhile; ?>
+						</div>
+<?php endif; ?>
+						<div class="col-xs-12 col-sm-9 col-sm-push-3">
+							<div class="page-content" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+								<h1><?php the_title(); ?></h1>
+								<?php the_content(); ?>
+								<hr>
+								<div class="sosyal">
+									<div class="sosyal1">
+										<a href="https://twitter.com/share" class="twitter-share-button" data-lang="tr">Tweet</a>
+										<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+									</div>
+									<div class="sosyal2">
+										<iframe src="//www.facebook.com/plugins/like.php?href=<?php the_permalink(); ?>&amp;width=468&amp;layout=standard&amp;action=like&amp;show_faces=false&amp;share=true&amp;height=20" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:468px; height:20px;" allowTransparency="true"></iframe>
+									</div>
+								</div>
+								<hr>
+								<p class="small text-muted"><span class="fa fa-time"></span> <?php the_date(); ?></p>
+							</div>
 						</div>
 						<div class="col-xs-12 col-sm-3 col-sm-pull-9">
 						</div>
@@ -29,6 +36,7 @@
 				</div>
 			</div>
 		</div><!-- main-content -->
+<?php endwhile; ?>
 <?php get_template_part( 'partials/section', 'cntlinks' ); ?>
 	</div><!-- site-content -->
 <?php get_footer(); ?>
