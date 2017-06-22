c2ea496dd520738aeac4410df1d82c5ea77fabae
Tanju Yildiz <ben@tanjuyildiz.com>
2017-06-22 14:28:46 +0300

    Arsiv sayfasi duzenlendi.

--- archive.php
+++ archive.php
@@ -5,7 +5,6 @@
 				<div class="container-fluid">
 					<div class="row">
 						<div class="col-xs-12 col-sm-9 col-sm-push-3">
-<?php if ( have_posts() ) : ?>
 							<div class="row">
 								<div class="col-xs-12">
 									<h1 class="page-header"><?php the_archive_title(); ?>
@@ -13,14 +12,19 @@
 									</h1>
 								</div>
 							</div>
+<?php if ( have_posts() ) : ?>
 <?php while ( have_posts() ) : the_post(); ?>
-							<div class="row">
-								<div class="col-xs-12 col-sm-5">
+							<div class="row archive-item">
+								<div class="col-xs-12 col-sm-4">
 									<a href="<?php the_permalink(); ?>">
-										<img class="img-responsive" src="http://placehold.it/700x300" alt="<?php the_title_attribute(); ?>">
+<?php if ( has_post_thumbnail() ): ?>
+										<?php the_post_thumbnail( "medium", [ "class" => "img-responsive", "alt" => get_the_title() ] ); ?>
+<?php else : ?>
+										<img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/images/default-thumb.jpg" alt="<?php the_title_attribute(); ?>">
+<?php endif; ?>
 									</a>
 								</div>
-								<div class="col-xs-12 col-sm-7">
+								<div class="col-xs-12 col-sm-8">
 									<h3><?php the_title(); ?></h3>
 									<p><?php the_excerpt(); ?></p>
 									<a class="btn btn-primary" href="<?php the_permalink(); ?>">Devamını Oku <span class="fa fa-angle-right"></span></a>
@@ -28,6 +32,11 @@
 							</div>
 <?php endwhile; ?>
 <?php else : ?>
+							<div class="row">
+								<div class="col-xs-12">
+									<p>Bu kategoriye henüz yazı eklenmemiş.</p>
+								</div>
+							</div>
 <?php endif; ?>
 						</div>
 						<div class="col-xs-12 col-sm-3 col-sm-pull-9">
