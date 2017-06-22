3798f139a7180f6e398dea9841ea67987f41e2dd
Tanju Yildiz <ben@tanjuyildiz.com>
2017-06-22 13:23:39 +0300

    404 sayfasi duzenlendi.

--- 404.php
+++ 404.php
@@ -1,64 +1,25 @@
-<?php
-/**
- * The template for displaying 404 pages (not found)
- *
- * @link https://codex.wordpress.org/Creating_an_Error_404_Page
- *
- * @package Parsel_Emlak
- */
-
-get_header(); ?>
-
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'parsel' ); ?></h1>
-				</header><!-- .page-header -->
-
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'parsel' ); ?></p>
-
-					<?php
-						get_search_form();
-
-						the_widget( 'WP_Widget_Recent_Posts' );
-
-						// Only show the widget if site has multiple categories.
-						if ( parsel_categorized_blog() ) :
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'parsel' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-						endif;
-
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'parsel' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-						the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php
-get_footer();
+<?php get_header(); ?>
+	<div class="site-content">
+		<div class="section main-content">
+			<div class="wrapper">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-xs-12 col-sm-6 col-sm-offset-3">
+							<div class="page-content not-found">
+								<h1 class="page-title">Sayfa bulunamadı.</h1>
+								<hr>
+								<p class="p404">404</p>
+								<hr>
+								<p>Ulaşmak istediğiniz sayfa bulunamadı. Sayfa değiştirilmiş veya silinmiş olabilir.</p>
+								<p>İsterseniz <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><strong>anasayfa</strong></a>'ya dönerek siteyi dolaşmaya devam edebilir ya da arama yaparak benzer sonuçlar bulabilirsiniz.</p>
+								<hr>
+								<?php get_search_form(); ?>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div><!-- main-content -->
+<?php get_template_part( 'partials/section', 'cntlinks' ); ?>
+	</div><!-- site-content -->
+<?php get_footer(); ?>
